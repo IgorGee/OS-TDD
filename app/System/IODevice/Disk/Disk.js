f0d0dd0f7167c5b849aad8dd91e80ba156745187
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-12-10 09:50:48 -0500

    Create Disk and Printer classes

---
+++ app/System/IODevice/Disk/Disk.js
@@ -0,0 +1,5 @@
+import IODevice from '../IODevice'
+
+export default class Disk extends IODevice {
+
+}
---
+++ app/System/IODevice/Printer/Printer.js
@@ -0,0 +1,5 @@
+import IODevice from '../IODevice'
+
+export default class Printer extends IODevice {
+  
+}
