e99e95a1cb6e2f1dd3329ddbb2a099bbc46024e5
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-12-10 07:48:14 -0500

    Add simple number validation

--- app/InitialPrompt/InitialPrompt.js
+++ app/InitialPrompt/InitialPrompt.js
@@ -3,21 +3,30 @@ export default class InitialPrompt {
     {
       type: 'input',
       name: 'ram',
-      message: 'RAM Capacity'
+      message: 'RAM Capacity',
+      validate: this.positiveNumberValidation
     },
     {
       type: 'input',
       name: 'disks',
-      message: 'Disk Quantity'
+      message: 'Disk Quantity',
+      validate: this.positiveNumberValidation
     },
     {
       type: 'input',
       name: 'printers',
-      message: 'Printer Quantity'
+      message: 'Printer Quantity',
+      validate: this.positiveNumberValidation
     }
   ]
 
   constructor() {
 
   }
+
+  positiveNumberValidation(input) {
+    if (!isNaN(input) && input > 0) return true
+    else return "Please enter a valid positive number"
+  }
+
 }
