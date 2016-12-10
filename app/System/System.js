42d17e8582c61d7024b7600eb9a5395fce1206b6
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-12-10 10:06:17 -0500

    Remove unecessary property declarations

--- app/System/System.js
+++ app/System/System.js
@@ -2,10 +2,6 @@ import Disk from './IODevice/Disk'
 import Printer from './IODevice/Printer'
 
 export default class System {
-  ramSize
-  disks
-  printerQuantity
-
   constructor(ramSize, diskQuantity, printerQuantity) {
     this.ramSize = ramSize
     this.disks = [...Array(diskQuantity)].map(() => new Disk)
