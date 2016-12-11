6ec885727c8d325773634112cd81d7fafe80497c
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-12-11 14:45:54 -0500

    Add prompts for system

--- app/Prompt/Prompt.js
+++ app/Prompt/Prompt.js
@@ -23,6 +23,38 @@ export default class Prompt {
     }
   ]
 
+  newProcess = [
+    {
+      type: 'input',
+      name: 'size',
+      message: 'Size:',
+      validate: this.positiveNumberValidation
+    },
+    {
+      type: 'input',
+      name: 'priority',
+      message: 'Priority:',
+      validate: this.positiveNumberValidation
+    }
+  ]
+
+  newIOOperation = [
+    {
+      type: 'input',
+      name: 'filename',
+      message: 'Filename:'
+    }
+  ]
+
+  listening = [
+    {
+      type: 'input',
+      name: 'command',
+      message: '>',
+      validate: this.commandValidation
+    }
+  ]
+
   async getSystem() {
     try {
       const { ram, disks, printers } = await Inquirer.prompt(this.initial)
@@ -35,6 +67,13 @@ export default class Prompt {
   positiveNumberValidation(input) {
     if (!isNaN(input) && input > 0) return true
     else return "Please enter a valid positive number"
+  }
+
+  commandValidation(input) {
+    const commands = /[At]|[DdPp]\d|Sr|Si|Sm/
+    const valid = commands.test(input)
+    if (valid) return valid
+    else return "That is not a valid command"
   }
 
 }
