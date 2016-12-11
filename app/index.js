2efe88da75ed225f9154cb11ffe1130778092f02
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-12-11 18:53:11 -0500

    Add checks to prevent non-existant IO devices

--- app/index.js
+++ app/index.js
@@ -14,23 +14,39 @@ const awaitCommand = async system => {
         break
       case 'p':
         const newPrinterNumber = parseInt(command.substring(1))
+        if (newPrinterNumber > system.printers.length) {
+          console.log(`P${newPrinterNumber} does not exist`)
+          break
+        }
         const pFilename = await (new Prompt).getFilename()
         if (!system.print(newPrinterNumber, pFilename))
           console.log('There are no processes in CPU or Ready Queue')
         break
       case 'P':
         const finishPrinterNumber = parseInt(command.substring(1))
+        if (finishPrinterNumber > system.printers.length) {
+          console.log(`P${finishPrinterNumber} does not exist`)
+          break
+        }
         if (!system.finishPrint(finishPrinterNumber))
           console.log('There are no processes using this IO device')
         break
       case 'd':
         const newDiskNumber = parseInt(command.substring(1))
+        if (newDiskNumber > system.disks.length) {
+          console.log(`D${newDiskNumber} does not exist`)
+          break
+        }
         const dFilename = await (new Prompt).getFilename()
         if (!system.diskIO(newDiskNumber, dFilename))
           console.log('There are no processes in CPU or Ready Queue')
         break
       case 'D':
         const finishDiskNumber = parseInt(command.substring(1))
+        if (finishDiskNumber > system.disks.length) {
+          console.log(`D${finishDiskNumber} does not exist`)
+          break
+        }
         if (!system.finishDiskIO(finishDiskNumber))
           console.log('There are no processes using this IO device')
         break
